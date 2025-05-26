5e635b79650656e365c11a3d935b13d0177ca96c
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-05-26 10:16:39 +0600

    create CreateUser component design

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import CreateOrder from './components/CreateOrder/CreateOrder'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
@@ -7,6 +8,9 @@ function App() {
     <>
       <div className="container mx-auto px-4 h-screen flex flex-col">
         <Navbar />
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6 flex-grow">
+          <CreateOrder />
+        </div>
       </div>
     </>
   )
