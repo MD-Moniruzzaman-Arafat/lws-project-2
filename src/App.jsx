f6b8a31f49005015bdad4f29a50520b9b09da4e7
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-05-26 12:14:47 +0600

    create OrderReport and OrderSummary component design

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import CreateOrder from './components/CreateOrder/CreateOrder'
 import Navbar from './components/Navbar/Navbar'
+import OrderReport from './components/OrderReport/OrderReport'
+import OrderSummary from './components/OrderSummary/OrderSummary'
 
 function App() {
 
@@ -10,6 +12,10 @@ function App() {
         <Navbar />
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6 flex-grow">
           <CreateOrder />
+          <div className="md:col-span-2 h-[calc(100vh_-_130px)]">
+            <OrderSummary />
+            <OrderReport />
+          </div>
         </div>
       </div>
     </>
