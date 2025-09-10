4f6793257999e08eaba894db3ef88d8cc4d99b9e
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-09-10 10:42:56 +0600

    navbar section component design

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   return (
     <>
-      <h1>hello</h1>
+      <div className="container mx-auto px-4 h-screen flex flex-col">
+        <Navbar />
+      </div>
     </>
   )
 }
